3e8bac6e84f5d2735ac3209eee054d82726a930b
hugocaptain <hugo@captain-team.net>
2021-10-21 23:58:22 +0200

    adding txtReader.write + parsers function +tests

--- src/model/Parser.java
+++ src/model/Parser.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Parser {
@@ -8,6 +9,7 @@ public class Parser {
 
     public  Parser() {
         this.map = new HashMap<String, Integer>();
+        map.put("010101111", 0);
         map.put("000001001", 1);
         map.put("010011110", 2);
         map.put("010011011", 3);
@@ -20,11 +22,13 @@ public class Parser {
     }
 
     public int parse(String value) {
+        System.out.println(value);
         if (map.containsKey(value)) {
             return map.get(value);
         }
         return -1;
     }
+
 
     public String decode(String stringToDecode) {
         String result = "";
@@ -35,6 +39,43 @@ public class Parser {
             }
             else {
                 result += "1";
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<String> arrayReader(ArrayList<String> array) {
+        ArrayList<String> results = new ArrayList<String>();
+        // System.out.println(array.get(0).length());
+        for(int j = 0; j < array.get(0).length(); j+=3) {
+            String s = "";
+            for(int i = 0; i< 3; i++) {
+                s += array.get(i).charAt(j);
+                s += array.get(i).charAt(j+1);
+                s += array.get(i).charAt(j+2);
+            }
+            results.add(s);
+        }
+        return results;
+    }
+    
+
+    public boolean checksum(ArrayList<Integer> values) {
+        int element = 0;
+        for(int i = 1; i <= values.size(); i++) {
+            // System.out.println(i + " * " + values.get(values.size()-i));
+            element += i*values.get(values.size()-i);
+        }
+        return element % 11 == 0;
+    }
+
+    public String intToString(ArrayList<Integer> values) {
+        String result = "";
+        for (int i : values) {
+            if(i == -1) {
+                result += "?";
+            } else {
+                result += String.valueOf(i);
             }
         }
         return result;
--- src/model/TxTReader.java
+++ src/model/TxTReader.java
@@ -2,16 +2,18 @@ package model;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class TxTReader {
 
     private static File file;
 
-    public static ArrayList<String> getData(String name) throws FileNotFoundException, IOException {
+    public static ArrayList<ArrayList<String>> read(String name) throws FileNotFoundException, IOException {
 
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
         ArrayList<String> data = new ArrayList<>();
-        //String fileName = getFileName(name);
-        file = new File(name+".txt");
+        file = new File(name);
 
         FileReader fr = new FileReader(file.getAbsoluteFile());
         BufferedReader br = new BufferedReader(fr);
@@ -19,11 +21,27 @@ public class TxTReader {
         String ligne;
 
         while((ligne = br.readLine()) != null){
-            data.add(ligne);
+            if(ligne.isBlank()) {
+                result.add(data);
+                data = new ArrayList<>();
+            } else {
+                data.add(ligne);
+            }
         }
         br.close();
 
-        return data;
+        return result;
+    }
+
+    public static void write(HashMap<String, Boolean> values) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+        System.out.println(values);
+        for(Entry<String, Boolean> value : values.entrySet()) {
+            String suffix = !value.getValue() ? "ERR" : "";
+            writer.println(value.getKey() + " " + suffix);
+        }
+        
+        writer.close();
     }
 }
 
